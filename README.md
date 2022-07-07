26d37e4ea772a587537f43b65db9f09fcd0928eb
ANANYA KANNAN <104369167+velvet-facade@users.noreply.github.com>
2022-07-07 23:31:31 +0530

    STAGE 9

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# PRO-C75-E-RIDE-STAGE-1
-https://code.whitehatjr.com/s/my-projects/view/87b73807-9782-4e5d-8d25-d7fa2b65cee1
+# PRO-C83-PROJECT
+After Class project for PRO-C83
--- components/BottomTabNavigator.js
+++ components/BottomTabNavigator.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
+import { View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 import RideScreen from "../screens/Ride";
 import RideHistoryScreen from "../screens/RideHistory";
@@ -11,26 +13,49 @@ export default class BottomTabNavigator extends Component {
   render() {
     return (
       <NavigationContainer>
-        <Tab.Navigator>
-          {/* 
-            <TabScreen name="Ride" component={RideScreen} />
-            <TabScreen name="Ride History" component={RideHistoryScreen} />
-          */}
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
 
-          {/* 
-            <Tab.Screen name="Ride" />
-            <Tab.Screen name="Ride History" />
-          */}
+              if (route.name === "Ride") {
+                iconName = "bicycle";
+              } else if (route.name === "Ride History") {
+                iconName = "time";
+              }
 
-          {/* 
-            <Screen name="Ride" component={RideScreen} />
-            <Screen name="Ride History" component={RideHistoryScreen} />
-          */}
-
-          
-            <Tab.Screen name="Ride" component={RideScreen} />
-            <Tab.Screen name="Ride History" component={RideHistoryScreen} />
-          
+              // You can return any component that you like here!
+              return (
+                <Ionicons
+                  name={iconName}
+                  size={size}
+                  color={color}
+                  size={size}
+                />
+              );
+            }
+          })}
+          tabBarOptions={{
+            activeTintColor: "#FBE5C0",
+            inactiveTintColor: "black",
+            style: {
+              height: 100,
+              borderTopWidth: 0,
+              backgroundColor: "#F88379"
+            },
+            labelStyle: {
+              fontSize: 20,
+              fontFamily: "Rajdhani_600SemiBold"
+            },
+            labelPosition: "below-icon",
+            tabStyle: {
+              alignItems: "center",
+              justifyContent: "center"
+            }
+          }}
+        >
+          <Tab.Screen name="Ride" component={RideScreen} />
+          <Tab.Screen name="Ride History" component={RideHistoryScreen} />
         </Tab.Navigator>
       </NavigationContainer>
     );
